3c62ef308516ea91f69021f4d0325b36f46da9af
Giovanni <gi.gonzalezm@duocuc.cl>
2024-12-01 21:47:18 -0300

    Creado El Apartado del crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor(private menu: MenuController) {}
+export class AppComponent implements OnInit {
+  id_rol: number | null = null;
 
-  openMenu() {
-    this.menu.open(); // Abre el menú lateral
+  constructor(
+    private menu: MenuController,
+    private router: Router,
+    private storage: NativeStorage
+  ) {}
+
+  async ngOnInit() {
+    // Obtener el rol del usuario desde NativeStorage
+    try {
+      this.id_rol = await this.storage.getItem('id_rol');
+    } catch (error) {
+      console.error('Error al obtener el rol del usuario:', error);
+      this.id_rol = null; // En caso de error, asignar null
+    }
   }
 
-  closeMenu() {
-    this.menu.close(); // Cierra el menú lateral
+  async navigateTo(path: string) {
+    // Cierra el menú lateral antes de redirigir
+    await this.menu.close();
+    this.router.navigate([path]);
   }
 }
